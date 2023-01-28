9f70f9986961f5b139a2419912250072b02c45a3
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-01-28 14:52:10 +0530

    Student name is made not null

--- models/student.js
+++ models/student.js
@@ -1,27 +1,28 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Student extends Model {
     static associate(models) {
       Student.belongsTo(models.Classroom, {
         foreignKey: 'classroom_id',
-        as: 'classroom'
-      });
+        as: 'classroom',
+      })
       Student.belongsToMany(models.Course, {
         through: 'StudentCourse',
         as: 'courses',
-        foreignKey: 'student_id'
-      });
+        foreignKey: 'student_id',
+      })
     }
-  };
-  Student.init({
-    classroom_id: DataTypes.INTEGER,
-    student_name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Student',
-  });
-  return Student;
-};
+  }
+  Student.init(
+    {
+      classroom_id: DataTypes.INTEGER,
+      student_name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Student',
+    }
+  )
+  return Student
+}
