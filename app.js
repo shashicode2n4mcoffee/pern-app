b8afa9a0d6fc9c4fbfd20e7c566f85901754ef63
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-01-28 15:45:33 +0530

    Cleaned the app file

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-// var indexRouter = require('./routes/index')
 let studentRouter = require('./routes/index').studentRouter
 let lectureRouter = require('./routes/index').lectureRouter
 let courseRouter = require('./routes/index').courseRouter
